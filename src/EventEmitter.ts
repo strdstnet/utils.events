6df26661519163d8464eb9e158fcdfe4df2dbe51
Bradley Watton <mail@bradleyw.me>
2020-11-02 00:19:23 +0000

    Fix events

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -2,54 +2,52 @@ import { Event } from './Event'
 
 import { EventEmitter as NEE } from 'events'
 
-type EventCb<E extends Event<any>> = (event: E) => void
+type EventCb<E extends Event<any> = Event<any>> = (event: E) => void
+type StaticEventCb<T, E extends Event<any> = Event<any>> = (obj: T, event: E) => void
 
-type EKey = string | symbol
+export type EKey = string | symbol
+export type EventDict = Record<EKey, Event<any>>
 
-interface IEventEmitter<Events extends Record<EKey, Event<any>>> {
+interface IEventEmitter<Events extends EventDict> {
   on(key: keyof Events, cb: EventCb<Events[typeof key]>): this,
   emit(key: keyof Events, event: Events[typeof key]): this,
 }
 
-interface IEventEmitterStatic<Events extends Record<EKey, Event<any>>> {
-  new(): IEventEmitter<Events>,
-  on(key: keyof Events, cb: EventCb<Events[typeof key]>): void,
+interface Constructor<M> {
+  new (...args: any[]): M,
 }
 
-export function EventEmitter<Events extends Record<EKey, Event<any>>>(): IEventEmitterStatic<Events> {
-  abstract class EventEmitter implements IEventEmitter<Events> {
+export abstract class EventEmitter<Events extends EventDict> implements IEventEmitter<Events> {
 
-    private static emitter: NEE = new NEE()
-    private emitter: NEE = new NEE()
+  private static emitter: NEE = new NEE()
+  private emitter: NEE = new NEE()
 
-    public on(key: keyof Events, cb: EventCb<Events[typeof key]>): this {
-      this.emitter.on(key as EKey, cb)
+  public on(key: keyof Events, cb: EventCb<Events[typeof key]>): this {
+    this.emitter.on(key as EKey, cb)
 
-      return this
-    }
-
-    public emit(key: keyof Events, event: Events[typeof key]): this {
-      this.emitters.forEach(e => e.emit(key as EKey, event))
-
-      return this
-    }
-
-    public static on(key: keyof Events, cb: EventCb<Events[typeof key]>): void {
-      this.emitter.on(key as EKey, cb)
-    }
-
-    private get static() {
-      return this.constructor as {
-        emitter: NEE,
-        new(): any,
-      }
-    }
-
-    private get emitters() {
-      return [this.emitter, this.static.emitter]
-    }
-
+    return this
   }
 
-  return EventEmitter as any as IEventEmitterStatic<Events>
+  public emit(key: keyof Events, event: Event<any>): this {
+    this.emitter.emit(key as EKey, event)
+    this.static.emitter.emit(key as EKey, this, event)
+
+    return this
+  }
+
+  public static on<T extends EventEmitter<any>>(this: Constructor<T>, key: string, cb: StaticEventCb<T>): void {
+    ((this as any).emitter as NEE).on(key as EKey, cb)
+  }
+
+  private get static() {
+    return this.constructor as {
+      emitter: NEE,
+      new(): any,
+    }
+  }
+
+  private get emitters() {
+    return [this.emitter, this.static.emitter]
+  }
+
 }
